6397e01298beaeb9060d000b5869cd57c01abbba
Oleg Troian <sleite4r@gmail.com>
2024-12-24 00:18:06 +0100

    create modal-window for img-Le-goût-infini

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ const allLinks = document.querySelectorAll("a");
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     const href = link.getAttribute("href");
-    
+
     if (href !== "#" && !href.startsWith("#")) {
       return; // Дозволяє перейти за звичайним посиланням
     }
@@ -80,3 +80,70 @@ allLinks.forEach(function (link) {
       headerEL.classList.toggle("nav-open");
   });
 });
+
+// MODAL-IMAGE-PLACAT
+
+// const modal = document.querySelector("#modal");
+// const modalImg = document.querySelector("#modal-img");
+
+// const closeModalBtt = document.querySelector(".close");
+// const imageLink = document.querySelectorAll(".link-image");
+
+// function openModal(imageSrc) {
+//   modalImg.src = imageSrc;
+//   modal.style.display = "block";
+// }
+
+// function closemodal() {
+//   modal.style.display = "none";
+//   modalImg.src = "";
+// }
+
+// imageLink.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const imageSrc = link.getAttribute("href");
+//     openModal(imageSrc);
+//   });
+// });
+
+// closeModalBtt.addEventListener("click", () => {
+//   closemodal();
+// });
+
+// window.addEventListener("click", (e) => {
+//   if (e.target === modal) {
+//     closemodal();
+//   }
+// });
+
+// Отримуємо модальне вікно
+const modal = document.getElementById("myModal");
+
+// Отримуємо <a> тег
+const link = document.getElementById("myLink");
+
+// Отримуємо кнопку закриття
+const span = document.getElementsByClassName("close")[0];
+
+// Додаємо зображення для модального вікна
+const modalImage = document.getElementById("modalImage");
+
+// При кліку на <a> відкриваємо модальне вікно і вставляємо зображення
+link.onclick = function (event) {
+  event.preventDefault(); // Забороняємо перехід за посиланням
+  modal.style.display = "block";
+  modalImage.src = "../img/Placat Le goût infini/Le-goût-infini-1.jpg"; // Замініть URL на своє зображення
+};
+
+// Закриваємо модальне вікно при кліку на "х"
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Закриваємо модальне вікно при кліку поза ним
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
