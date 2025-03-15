3cba8e4066650b2baaa975b3225cba5d73d50e9f
Oleg <sleite4r@gmail.com>
2025-03-15 21:08:53 +0100

    update link resume, update pdf file

---
+++ js/locales/heroLang.js
@@ -0,0 +1,33 @@
+export const heroContent = {
+  title: {
+    en: "Hi, I'm Oleg.",
+    dk: "Hej, jeg er Oleg.",
+    ua: "Привіт, я Олег.",
+  },
+  subtitle: {
+    en: "I am a Web Developer",
+    dk: "Jeg er en webudvikler",
+    ua: "Я веб-розробник",
+  },
+  context: {
+    en: "I am currently studying web development at Viborg Media College and looking for a summer internship to improve my skills and gain hands-on experience.",
+    dk: "Jeg studerer i øjeblikket webudvikling på Viborg Media College og leder efter et sommerpraktik for at forbedre mine færdigheder og få praktisk erfaring.",
+    ua: "Наразі я навчаюся веб-розробці в Viborg Media College і шукаю літню практику, щоб покращити свої навички та здобути практичний досвід.",
+  },
+  cv: {
+    text: {
+      en: "Get My CV",
+      dk: "Hent mit CV",
+      ua: "Отримати моє резюме",
+    },
+    href: "https://drive.google.com/uc?export=download&id=1w4lw9mc5_z2SWZXibFLDPBNtFLwdB4kA",
+  },
+  github: {
+    text: {
+      en: "View GitHub",
+      dk: "Se GitHub",
+      ua: "Переглянути GitHub",
+    },
+    url: "https://github.com/sleiterr",
+  },
+};
---
+++ js/locales/navLang.js
@@ -0,0 +1,62 @@
+export const navBarContent = [
+  {
+    id: "home",
+    text: {
+      en: "Home",
+      dk: "Hjem",
+      ua: "Головна",
+    },
+    href: "#home",
+  },
+
+  {
+    id: "about",
+    text: {
+      en: "About",
+      dk: "Om",
+      ua: "Про мене",
+    },
+    href: "#about",
+  },
+
+  {
+    id: "services",
+    text: {
+      en: "Studies",
+      dk: "Studier",
+      ua: "Навчання",
+    },
+    href: "#services",
+  },
+
+  {
+    id: "experience",
+    text: {
+      en: "Experience",
+      dk: "Erfaring",
+      ua: "Досвід",
+    },
+    href: "#experience",
+  },
+
+  {
+    id: "Skills",
+    text: {
+      en: "Skills",
+      dk: "Færdigheder",
+      ua: "Навички",
+    },
+    href: "#skills",
+  },
+
+  {
+    id: "portfolio",
+    text: {
+      en: "Portfolio",
+      dk: "Portefølje",
+      ua: "Портфоліо",
+    },
+    href: "./gallery.html",
+    target: "_blank",
+  },
+];
